b8c8b99692a2a991b17136468e64580e20950b36
pc <pc@gmail.com>
2015-06-13 01:41:15 +0530

    dashboard

--- src/com/littleutil/BaseActivity.java
+++ src/com/littleutil/BaseActivity.java
@@ -4,13 +4,22 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import com.littleutil.application.Appsettings;
+import com.littleutil.application.UserInfo;
+
 public class BaseActivity extends Activity {
 
 	public ProgressDialog dialog;
+	public Appsettings app = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		app = (Appsettings)getApplication();
+		if(!app.init){
+			app.init = true;
+			app.setUserinfo(new UserInfo(this));
+		}
 	}
 
 	public void doShowLoading() {
