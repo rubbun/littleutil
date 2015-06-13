d84e8eb14a86e7fe7c1f1cf0b20dfcfba99450b6
Yajneshwar Mnadal <yajneshwar.mandal@android.com>
2015-06-13 10:36:16 +0530

    request submit activity created

---
+++ src/com/littleutil/screens/RequestSubmitActivity.java
@@ -0,0 +1,14 @@
+package com.littleutil.screens;
+
+import android.os.Bundle;
+
+import com.littleutil.BaseActivity;
+
+public class RequestSubmitActivity extends BaseActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
